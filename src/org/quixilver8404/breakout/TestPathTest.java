4c526308fb477ce01e8296659e3193829f6e3f89
Andrea Baretta <andrea.m.baretta@gmail.com>
2021-01-29 16:52:58 -0800

    Add test time-parametric path

--- src/org/quixilver8404/breakout/TestPathTest.java
+++ src/org/quixilver8404/breakout/TestPathTest.java
@@ -1,6 +1,7 @@
 package org.quixilver8404.breakout;
 
 import org.quixilver8404.breakout.controller.Breakout;
+import org.quixilver8404.breakout.controller.TestPath;
 import org.quixilver8404.breakout.simulator.Display;
 import org.quixilver8404.breakout.simulator.MecanumKinematics;
 import org.quixilver8404.breakout.util.Config;
@@ -9,29 +10,27 @@ import org.quixilver8404.breakout.util.Vector3;
 import java.io.File;
 import java.util.ArrayList;
 
-public class BreakoutTest {
-
+public class TestPathTest {
     public static void main(final String[] args) {
         Display window1 = new Display(1000, 1000, 100);
         window1.init();
 
-        final Breakout breakout = new Breakout(new File("/home/andrea/Desktop/foxtrotFiles/power_shot_and_wobble_goals_blue_breakout2.foxtrot2"), 1, new ArrayList<>(),
-                new Config(12, 12,11.2, -11.2, 1.17749, 0.01, 2.1, 31.4, 20,
-                        37.5/1000, MecanumKinematics.FindMomentOfInertia(0.5, 0.5, 20), 0.4572/2, 0.4572/2, 0.95,
-                        0.95, Math.PI/2.5, 0.2, 0.1));
+        final TestPath testPath = new TestPath(0.6, new Config(12, 12,11.2, -11.2, 1.17749, 0.01, 2.1, 31.4, 20,
+                37.5/1000, MecanumKinematics.FindMomentOfInertia(0.5, 0.5, 20), 0.4572/2, 0.4572/2, 0.95,
+                0.95, Math.PI/2.5, 0.2, 0.1));
 
         final MecanumKinematics kinematics = new MecanumKinematics(50, Config.MASS, 0.5, 0.5, new Vector3(0,0,0),
-                new Vector3(breakout.path.startX, breakout.path.startY,breakout.path.startHeading - Math.PI/2), window1, Config.J, Config.r_X, Config.r_Y, Config.T_MAX,
+                testPath.last_pos, window1, Config.J, Config.r_X, Config.r_Y, Config.T_MAX,
                 Config.WHEEL_RADIUS, Config.OMEGA_MAX);
 
 //        System.out.println(breakout == null);
 //        System.out.println(breakout.path == null);
         while (true) {
-            final double[] powerSettings = breakout.iterate(kinematics.getFieldPos(), kinematics.getFieldVel(), 0.0002);
+            final double[] powerSettings = testPath.iterate(kinematics.getFieldPos(), kinematics.getFieldVel(), 0.0002);
             kinematics.update(powerSettings, 0.0002);
-            kinematics.ui.drawCircle(breakout.getLastDesiredPos().x, breakout.getLastDesiredPos().y, 0.03, 100, new double[]{0,255,0});
+            kinematics.ui.drawCircle(testPath.last_pos.x, testPath.last_pos.y, 0.03, 100, new double[]{0,255,0});
             kinematics.ui.drawCircle(0, 0, 0.03, 100, new double[]{255,0,0});
-            kinematics.ui.drawCompassPixel(breakout.getLastDesiredPos().theta, 0, 0, 50);
+            kinematics.ui.drawCompassPixel(testPath.last_pos.theta, 0, 0, 50);
             kinematics.ui.update();
         }
     }
---
+++ src/org/quixilver8404/breakout/controller/TestPath.java
@@ -0,0 +1,46 @@
+package org.quixilver8404.breakout.controller;
+
+import org.quixilver8404.breakout.feedforward.RobotState;
+import org.quixilver8404.breakout.util.Config;
+import org.quixilver8404.breakout.util.Vector3;
+
+public class TestPath {
+
+    final double v;
+    protected final Config config;
+    public Controller controller;
+    public final PowerProfile powerProfile;
+
+    public TestPath(final double vel, final Config config) {
+        v = vel;
+        this.config = config;
+        controller = new Controller(Controller.computeK(Config.MASS, Config.WHEEL_RADIUS, Config.J, Config.OMEGA_MAX, Config.T_MAX, Config.r_X, Config.r_Y));
+        powerProfile = new PowerProfile(Config.MASS, Config.WHEEL_RADIUS, Config.J, Config.OMEGA_MAX, Config.T_MAX, Config.r_X, Config.r_Y, true);
+    }
+
+    protected double t = 0;
+
+    public Vector3 last_pos = new Vector3(1,0,0);
+
+    public double[] iterate(final Vector3 pos, final Vector3 vel, final double dt) {
+
+        final Vector3 des_pos = new Vector3(Math.cos(v*t), Math.sin(v*t), -Math.PI/2);
+        final Vector3 des_vel = new Vector3(-Math.sin(v*t)*v, Math.cos(v*t)*v, 0);
+        final Vector3 des_acc = new Vector3(-Math.cos(v*t)*v*v, -Math.sin(v*t)*v*v, 0);
+
+        last_pos = des_pos;
+
+        final double[] correction = controller.correction(Vector3.subtractVector2(pos, des_pos),
+                Vector3.subtractVector(vel, des_vel));
+
+        final double[] powerSettings = powerProfile.powerSetting(des_acc, des_vel, correction, pos.theta);
+
+        final double[] frictionAdjustedPowerSettings;
+
+        frictionAdjustedPowerSettings = FrictionCorrection.correction(vel, Vector3.subtractVector2(pos, des_pos), pos.theta, powerSettings, true);
+
+        t += dt;
+
+        return frictionAdjustedPowerSettings;
+    }
+}
